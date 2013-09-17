894ba725d2d784a5b034d9893db0ac07da0b9997
Rory McKinley <rorymckinley@gmail.com>
2013-09-17 21:53:10 +0200

    Initial Rails Setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SchoolnetworkSearchPrototype::Application.config.secret_key_base = 'afea0c70d2f230b6fc3e87f52919580f97326f6f356d9ecedbe9cb9b7bc8316f342a8eb760335f0f251bf7e465b59cfead18de00c0c6711ba46712a298107437'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SchoolnetworkSearchPrototype::Application.config.session_store :cookie_store, key: '_schoolnetwork_search_prototype_session'
