75675f4bea900e15bab274b58f26f659659488c3
Rory McKinley <rorymckinley@gmail.com>
2013-10-08 23:20:27 +0200

    Trim uploaded site names

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -9,7 +9,7 @@ class SitesController < ApplicationController
 
   def bulk_create
     params[:sites].split("\n").each do |site_name|
-      Site.create name: site_name
+      Site.create name: site_name.chomp
     end
     redirect_to :sites
   end
